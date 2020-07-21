fbf438db9553f873cdb6e4bc5fc9aefbc0bd3a09
amitsou <amitsou95@gmail.com>
2020-07-21 13:49:07 +0300

    First Commit for FSR

---
+++ Arduino_FSR/version1/version1.ino
@@ -0,0 +1,77 @@
+#include<TimeLib.h>
+
+
+/*Force Sensor Resistance to Arduino's Analog Ports*/
+int fsrAnalogPin1 = 0; //FSR1 is connected to analog 0
+int fsrAnalogPin2 = 1; //FSR2 is connected to analog 1
+int fsrAnalogPin3 = 2; //FSR3 is connected to analog 2
+int fsrAnalogPin4 = 3; //FSR4 is connected to analog 3
+int fsrAnalogPin5 = 4; //FSR5 is connected to analog 4
+
+/*LED connections to Arduino's Digital Ports*/
+int LEDpin1 = 11; //1.Green LED to pin 11 (PWM pin)
+int LEDpin2 = 10; //2.Yellow LED to pin 10 (PWM pin)
+int LEDpin3 = 9; //3.Blue LED to pin 9 (PWM pin)
+int LEDpin4 = 8 ; //4.White LED to pin 8 (PWM pin)
+int LEDpin5 = 12; //5. Orange LED to pin 12 (PWM pin)
+
+/*The analog reading from the FSR resistor divider*/
+int fsrReading1;
+int fsrReading2;
+int fsrReading3;
+int fsrReading4;
+int fsrReading5;
+int LEDbrightness;
+ 
+void setup(void) {
+  Serial.begin(9600);   /*Send debugging information via the Serial monitor*/
+  pinMode(LEDpin1, OUTPUT);
+  pinMode(LEDpin2, OUTPUT);
+  pinMode(LEDpin3, OUTPUT);
+  pinMode(LEDpin4, OUTPUT);
+  pinMode(LEDpin5, OUTPUT);
+}
+ 
+void loop(void) {  
+ 
+   fsrReading1 = analogRead(fsrAnalogPin1);
+   Serial.print("A0 : ");
+   Serial.println(fsrReading1);
+   //Serial.println("");
+
+   fsrReading2 = analogRead(fsrAnalogPin2);
+   Serial.print("A1 : ");
+   Serial.println(fsrReading2);
+   //Serial.println("");
+
+   fsrReading3 = analogRead(fsrAnalogPin3);
+   Serial.print("A2 : ");
+   Serial.println(fsrReading3);
+   //Serial.println("");
+
+   fsrReading4 = analogRead(fsrAnalogPin4);
+   Serial.print("A3 : ");
+   Serial.println(fsrReading4);
+   //Serial.println("");
+
+   fsrReading5 = analogRead(fsrAnalogPin5);
+   Serial.print("A4 : ");
+   Serial.println(fsrReading5);
+   //Serial.println("");
+ 
+   /* We'll need to change the range from the analog reading (0-1023) down to the range used by analogWrite (0-255) with map! */  
+   /* LED gets brighter or harder depending on how it is pressed by the user */
+   LEDbrightness = map(fsrReading1, 0, 1023, 0, 255);
+   analogWrite(LEDpin1, LEDbrightness);
+   LEDbrightness = map(fsrReading2, 0, 1023, 0, 255);
+   analogWrite(LEDpin2, LEDbrightness);
+   LEDbrightness = map(fsrReading3, 0, 1023, 0, 255);
+   analogWrite(LEDpin3, LEDbrightness);
+   LEDbrightness = map(fsrReading4, 0, 1023, 0, 255);
+   analogWrite(LEDpin4, LEDbrightness);
+   LEDbrightness = map(fsrReading5, 0, 1023, 0, 255);
+   analogWrite(LEDpin5, LEDbrightness);
+
+   //digitalClockDisplay();
+   delay(1000);
+}
