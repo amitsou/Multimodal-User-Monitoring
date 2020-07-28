44fbce7711570eb1ddb48497525bab60e91fb4fc
amitsou <amitsou95@gmail.com>
2020-07-28 11:48:02 +0300

    updated mouse logger with txt to csv converter

---
+++ mouse_logger/txt_to_csv.py
@@ -0,0 +1,18 @@
+#test file
+
+import argparse
+import pandas as pd
+
+def convert_txt_to_csv(path,newpath,fname):
+
+    filename=
+    path = 
+    df = pd.read_fwf(path)
+
+    filename = fname+".csv"
+    newpath=path
+    names = ["Date","Time","Action","PosX","PosY","Button"]
+    df.to_csv(newpath,header=names)
+    return 
+
+
