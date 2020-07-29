7463ad1919652f045d42de28f19edf2051bdc8c7
amitsou <amitsou95@gmail.com>
2020-07-29 16:09:22 +0300

    Update serial_out.py

--- Force_Resistance_Sensor/serial_out.py
+++ Force_Resistance_Sensor/serial_out.py
@@ -1,9 +1,12 @@
 import sys
 import time
 import serial 
+import logging
+from serial import Serial
 from time import sleep
 from datetime import datetime
 
+logging.basicConfig(filename="serialOut.txt",level=logging.DEBUG,format="%(asctime)s    %(message)s")
 ser = serial.Serial('/dev/ttyACM0', 9600)
 f = open('alekos.txt','a+')
 i = 0 
@@ -12,23 +15,14 @@ flag = False
 while True:
     try:        
         data_raw = str(ser.readline().decode().strip('\r\n'))
-        tmp = data_raw.split(':')[0]
-        
-        if(tmp == 'A0 '):
-            now = datetime.now()
+        tmp = data_raw.split('  ')[0]
+        if(tmp == 'A0'):
             flag = True
-            i+=1
-        print(data_raw)
-        #write to file
-        f.write(data_raw)
-        f.write("\n")
-        if(flag and tmp == 'A4 '):
-            print()
+        if (flag and tmp != 'A4'):
+            print(data_raw) 
+            logging.info(data_raw)
+        if(flag and tmp == 'A4'):
             flag = False
-            print(now)
-            #write to file 
-            f.write(str(now))
-            f.write("\n")
-            
+            logging.info(data_raw)     
     except UnicodeDecodeError:
         print("UnicodeDecodeError!")
