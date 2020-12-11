fbfa0f386a1017f992ec99eff2051bb3b21dcedc
amitsou <amitsou95@gmail.com>
2020-12-11 02:43:47 +0200

    Upgraded auxiliary files

---
+++ Data_Collection/parser_scheduler.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import time
+import subprocess
+import sys
+
+if __name__=='__main__':
+    parser = "/home/alex/Desktop/Test_Data/Parsers/csv_parser.py"
+    subprocess.run(parser, shell=True)
