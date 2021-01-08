a0ad9761e2f186e04dfb193b38bf9613795929e7
Alexander Mitsou <amitsou95@gmail.com>
2021-01-08 17:34:16 +0200

    Update webcamlogger.sh

--- Data_Collection/Loggers/webcamlogger.sh
+++ Data_Collection/Loggers/webcamlogger.sh
@@ -1,46 +1,23 @@
 #!/bin/bash
 
-#1.Creating video-audio folder
-parent_dir=`dirname \`pwd\`` #Multimodal_User_Monitoring
+parent_dir=`dirname \`pwd\`` #Test_Data
 folder_name="/Data/Webcam" #The folder where audio and video is going to be saved
-new_path=$parent_dir$folder_name #Concatenating current working directory and the video audio folder
+new_path=$parent_dir$folder_name
 
-#Checkig for Webcam folder existance
-if [ -d "$new_path" ]; then
-    echo "video_audio folder exists..."
-else
-    ### Create Webcam directory ###
-    #echo
-    #echo "Error: ${new_path} not found..."
-    #echo "Creating video_audio folder in the current directory..."
-    mkdir -p -- "$new_path"
-    sudo chmod 777 "$new_path"
-    #echo "Folder created"
-    #echo
-fi
+now=$(date +%F)
+now="$( echo -e "$now" | tr  '-' '_'  )"
+sub_dir=$new_path'/'$now
 
-#2.Creating a sub-directory into video_audio folder
-# Every sub-dir will be named based on the (current) date
-now=$(date +%F) #Date needs to be formatted in YYYY-MM-DD style
-now="$( echo -e "$now" | tr  '-' '_'  )" #Replacing '-' with '_'
-sub_dir=$new_path'/'$now #Creating new path
-
-#Checkig for sub-folder existance, based on date!
 if [ -d "$sub_dir" ]; then
-    #echo "Date Sub-directory exists..."
+    echo "Date Sub-directory exists..."
     echo
 else
-    ### Create sub-folder ###
-    #echo "Error: ${sub_dir} not found..."
-    #echo "Creating date sub-directory..."
     mkdir -p -- "$sub_dir"
-    sudo chmod 777 "$sub_dir"
-    #echo "Date sub-directory created..."
-    #echo
+    chmod 777 "$sub_dir"
+    echo "Date sub-directory created..."
 fi
 
-
-#4.Recording audio and video
-fname=$(date +%H_%M_%S)".avi" #Webcam video is named based on the current time
+fname=$(date +%H_%M_%S)".avi"
 video_dir=$sub_dir'/'$fname
-ffmpeg -f pulse -ac 1 -i default -f v4l2 -i  /dev/video0 -vcodec libx264 -t 00:01:00 $video_dir #Record for a minute
+ffmpeg -f pulse -ac 1 -i default -f v4l2 -i  /dev/video0 -vcodec libx264 -t 00:01:00 $video_dir >/dev/null 2>&1 #Record for a minute and prevent ffmpeg stdout
+echo "Exiting Webcamlogger"
