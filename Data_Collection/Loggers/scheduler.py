da78a3b01ba7f2bc611293f7ff31b33a3c3d65ea
Alexander Mitsou <amitsou95@gmail.com>
2021-01-08 22:48:47 +0200

    Update scheduler.py

--- Data_Collection/Loggers/scheduler.py
+++ Data_Collection/Loggers/scheduler.py
@@ -10,14 +10,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
 from Functions import utils as ut
 
-
-def time_in_range(start, end, x):
-    """Return true if x is in the range [start, end]"""
-    if start <= end:
-        return start <= x <= end
-    else:
-        return start <= x or x <= end
-
 def stop_scheduler():
     print('{} Exitting... '.format(datetime.datetime.now()))
     sys.exit()
@@ -27,9 +19,7 @@ def get_data():
     subprocess.Popen(["/home/alex/Desktop/Test_Data/Loggers/scripts.sh"], stdin=subprocess.PIPE)
 
 
-
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Collect arguments')
     #parser.add_argument("--start", metavar='start(text)', help="Please provide start time in hh:mm:ss format")
     parser.add_argument("--stop", metavar='stop(text)', help="Please provide end time in hh:mm:ss format")
@@ -39,16 +29,18 @@ if __name__ == '__main__':
     #start_time = args.start
     start_time = str(datetime.datetime.now().time()).split('.')[0]
     end_time = args.stop
+    minutes = args.m
+
     start_h, start_m, start_s = start_time.split(':')
     end_h, end_m, end_s = end_time.split(':')
     start = datetime.time(int(start_h), int(start_m), int(start_s))
     end = datetime.time(int(end_h), int(end_m), int(end_s))
-    minutes = args.m
+
 
     schedule.every(int(minutes)).minutes.do(get_data)
     while True:
-        if time_in_range(start, end, datetime.datetime.now().time()):
+        if ut.time_in_range(start, end, datetime.datetime.now().time()):
             schedule.run_pending()
-            time.sleep(120)
+            time.sleep(60)
         else:
             stop_scheduler()
