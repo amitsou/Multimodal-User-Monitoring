8b614fec9c52a2f1a755237bbda6446c7e0b28f1
Alexander Mitsou <amitsou95@gmail.com>
2021-01-13 23:53:20 +0200

    Update scheduler.py

--- Data_Collection/Loggers/scheduler.py
+++ Data_Collection/Loggers/scheduler.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-''' usage: python3 scheduler.py --stop 12:00:00 --m 2 '''
+import logging
 import datetime
 import schedule
 import argparse
@@ -7,20 +7,26 @@ import subprocess
 import sys
 import time
 import os
+
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
+
 from Functions import utils as ut
 
+
 def stop_scheduler():
-    print('{} Exitting... '.format(datetime.datetime.now()))
+    print('{} Data Collection Completed, Exitting Loggers... '.format(datetime.datetime.now()))
     sys.exit()
 
 def get_data():
     print('{} Executing scripts...'.format(datetime.datetime.now()))
-    subprocess.Popen(["/home/alex/Desktop/Test_Data/Loggers/scripts.sh"], stdin=subprocess.PIPE)
-
+    #subprocess.Popen(["/home/alex/Desktop/Test_Data/Loggers/scripts.sh"], stdin=subprocess.PIPE)
+    subprocess.Popen(["scripts.sh"], stdin=subprocess.PIPE)
 
 if __name__ == '__main__':
+
+    logging.getLogger('schedule').propagate = False #disable schedule stdout
+
     parser = argparse.ArgumentParser(description='Collect arguments')
     parser.add_argument("--stop", metavar='stop(text)', help="Please provide end time in hh:mm:ss format")
     parser.add_argument("--m", metavar='m(text)', help="Please provide the minutes in order to execute each job")
@@ -29,12 +35,10 @@ if __name__ == '__main__':
     start_time = str(datetime.datetime.now().time()).split('.')[0]
     end_time = args.stop
     minutes = args.m
-
     start_h, start_m, start_s = start_time.split(':')
     end_h, end_m, end_s = end_time.split(':')
     start = datetime.time(int(start_h), int(start_m), int(start_s))
     end = datetime.time(int(end_h), int(end_m), int(end_s))
-
 
     schedule.every(int(minutes)).minutes.do(get_data)
     while True:
@@ -43,3 +47,9 @@ if __name__ == '__main__':
             time.sleep(60)
         else:
             stop_scheduler()
+            current_path = os.path.abspath(os.getcwd())
+            os.chdir('..')
+            current_path = (os.path.abspath(os.curdir))
+            current_path = os.path.join(current_path,'Parsers')
+            parser = '/'.join((current_path,'csv_parser.py'))
+            subprocess.run(parser, shell=True)
