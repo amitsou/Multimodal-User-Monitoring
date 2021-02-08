a8bc9ca8879fcb6df828e726e05e8a0b88219179
Alexander Mitsou <amitsou95@gmail.com>
2021-02-08 21:42:01 +0200

    Update keylogger.py

--- src/Loggers/keylogger.py
+++ src/Loggers/keylogger.py
@@ -7,7 +7,6 @@ import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
-
 from Functions import utils as ut
 
 if __name__=='__main__':
@@ -23,5 +22,5 @@ if __name__=='__main__':
     except KeyboardInterrupt as err:
         print(err)
         sys.exit(0)
-
+        
     print('Exiting logger...')
