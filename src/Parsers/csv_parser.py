6143a5e5fafe91c6c3b86a163c709fb387cd0b7b
Alexander Mitsou <amitsou95@gmail.com>
2021-02-08 21:40:36 +0200

    Update csv_parser.py

--- src/Parsers/csv_parser.py
+++ src/Parsers/csv_parser.py
@@ -2,10 +2,8 @@
 import shutil
 import sys
 import os
-
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
-
 from Functions import utils as ut
 
 if __name__ == '__main__':
