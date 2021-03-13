012a52925a854868cf2443d7dbb91cd8c506c903
Alexander Mitsou <amitsou95@gmail.com>
2021-03-13 18:40:01 +0200

    Update extract_features.py

--- src/Features/extract_features.py
+++ src/Features/extract_features.py
@@ -2,6 +2,7 @@
 """ usage: python3 feature_extraction.py --segment size 10"""
 import sys
 import os
+
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "../"))
 from Functions import feature_functions as ft
@@ -9,4 +10,11 @@ from Functions import utils as ut
 
 if __name__ == '__main__':
     segment_size = ut.parse_CLI()
+    #Create and save features in .csv files
     ft.extract_features(segment_size)
+
+    '''
+    #Create actions.csv, that contains the whole recordings in it
+    #in order to see the class distribution
+    ft.get_all_features() #Reminder: Set the appropriate directory
+    '''
