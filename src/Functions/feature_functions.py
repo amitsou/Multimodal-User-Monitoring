917635f891c56e40ee4a520c9cb5fda1f2c86252
Alexander Mitsou <amitsou95@gmail.com>
2021-03-13 18:40:44 +0200

    Update feature_functions.py

--- src/Functions/feature_functions.py
+++ src/Functions/feature_functions.py
@@ -6,39 +6,44 @@ from datetime import timedelta
 from functools import reduce
 import plotly.graph_objs as go
 import plotly as py
-import datetime
 import pandas as pd
 import numpy as np
+import collections
 import itertools
+import datetime
 import shutil
 import time
 import os
 import sys
 
+sys.path.insert(0, os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "../"))
+from Functions import utils as ut
 
+#TO DO: REVIEW get_recordings()
 def get_recordings():
     """Get the recorded .csv files
 
     Returns:
-        list: A list containing the absolute paths
+        list: A list containing the absolute dirs for the recorded .csv files
     """
     current_path = os.path.abspath(os.getcwd())
     os.chdir('..')
     current_path = (os.path.abspath(os.curdir))
 
-    mouse = os.path.join(current_path,'Data_Samples','Test_Action_1','Mouse','CSV')
+    mouse = os.path.join(current_path,'Debug','Mouse','CSV')
     mouse_files = []
     for f in os.listdir(mouse):
         tmp = '/'.join((mouse,f))
         mouse_files.append(tmp)
 
-    keyboard = os.path.join(current_path,'Data_Samples','Test_Action_1','Keyboard','CSV')
+    keyboard = os.path.join(current_path,'Debug','Keyboard','CSV')
     keyboard_files = []
     for f in os.listdir(keyboard):
         tmp = '/'.join((keyboard,f))
         keyboard_files.append(tmp)
 
-    chair = os.path.join(current_path,'Data_Samples','Test_Action_1','Chair','CSV')
+    chair = os.path.join(current_path,'Debug','Chair','CSV')
     chair_files = []
     for f in os.listdir(chair):
         tmp = '/'.join((chair,f))
@@ -56,8 +61,10 @@ def convert_seconds(seconds):
     Returns:
         list: A list containing the converted seconds into time format
     """
-    time = []
+    seconds = list(map(int,seconds))
     dt0 = datetime.datetime(1,1,1)
+
+    time = []
     for i,t in enumerate(seconds):
         h = int(t // 3600)
         m = int(t % 3600 // 60)
@@ -96,7 +103,7 @@ def get_seconds(timestamp,index):
     for i, t in enumerate(timestamp):
         split = t.split(':')
 
-        if len(split)==3:
+        if len(split) == 3:
             if i in tmp:
                 seconds = float(split[0]) * 3600 + float(split[1]) * 60 + float(split[2].replace(',', '.')) + tmp1
                 times_seconds.append(seconds)
@@ -109,6 +116,22 @@ def get_seconds(timestamp,index):
 
     del tmp
     return times_seconds
+
+
+def absoluteFilePaths(directory) -> list:
+    """Get the absolute paths given a directory
+
+    Args:
+        directory (str): The directory to examine
+
+    Returns:
+        list: A list containing the file names in absolute path format
+    """
+    l = []
+    for dirpath,_,filenames in os.walk(directory):
+        for f in filenames:
+             l.append(os.path.abspath(os.path.join(dirpath, f)))
+    return l
 
 
 def velocity(array):
@@ -139,10 +162,11 @@ def get_mouse_features(filename,segment_size):
     data = pd.read_csv(filename, delimiter=',')
     times = list(data['Time'])
     x, y, b = data['PosX'], data['PosY'], data['Button']
+
     day_start = data['Date'][0]
     next_dates = (data['Date'] > day_start)
 
-    if (next_dates.any()):
+    if next_dates.any():
         next_dates = data.loc[next_dates]
         next_day = int(next_dates.index[0])
         times_seconds = get_seconds(times,next_day)
@@ -156,7 +180,12 @@ def get_mouse_features(filename,segment_size):
         'Button.middle'
     ]
 
-    start = times_seconds[0]
+    #start = times_seconds[0]
+    tmp = ut.splitall(filename) #get the first timestamp from the filename
+    tmp = tmp[-1]
+    start = tmp[11:-4].split('_')
+    start = float(start[0]) * 3600 + float(start[1]) * 60 + float(start[2])
+
     features = []
     segment_centers = []
     clicks = r_clicks = l_clicks = m_clicks = 0
@@ -169,6 +198,7 @@ def get_mouse_features(filename,segment_size):
 
         velocity_x = velocity(cur_x)
         velocity_y = velocity(cur_y)
+
         for index,button in enumerate(cur_b):
             if button == 'None':
                 pass
@@ -185,6 +215,7 @@ def get_mouse_features(filename,segment_size):
                          l_clicks/segment_size, m_clicks/segment_size])
         segment_centers.append(start + segment_size / 2)
         start += segment_size
+
     features = np.array(features)
     segment_centers = np.array(segment_centers)
     return features, segment_centers, day_start
@@ -203,10 +234,11 @@ def get_key_features(filename,segment_size):
     data = pd.read_csv(filename, delimiter=',')
     keys = data['Key']
     times = list(data['Time'])
+
     day_start = data['Date'][0]
     next_dates = (data['Date'] > day_start)
 
-    if (next_dates.any()):
+    if next_dates.any():
         next_dates = data.loc[next_dates]
         next_day = int(next_dates.index[0])
         times_seconds = get_seconds(times,next_day)
@@ -220,7 +252,12 @@ def get_key_features(filename,segment_size):
         'Key.space','Key.enter','Key.page_down','Key.page_up'
     ]
 
-    start = times_seconds[0]
+    #start = times_seconds[0]
+    tmp = ut.splitall(filename)
+    tmp = tmp[-1]
+    start = tmp[11:-4].split('_')
+    start = float(start[0]) * 3600 + float(start[1]) * 60 + float(start[2])
+
     features = []
     segment_centers = []
     all_keys = arrow_keys = spaces = shift_ctrl_alt = 0
@@ -252,7 +289,7 @@ def get_key_features(filename,segment_size):
 
 
 def get_chair_features(filename,segment_size):
-    """Chair FRS feature extraction
+    """Chair FRS  feature extraction
 
     Args:
         filename (str): CSV recordings file
@@ -264,6 +301,7 @@ def get_chair_features(filename,segment_size):
     data = pd.read_csv(filename, delimiter=',')
     times = list(data['Time'])
     a0, a1, a2, a3, a4 = data['A0'], data['A1'], data['A2'], data['A3'], data['A4']
+
     day_start = data['Date'][0]
     next_dates = (data['Date'] > day_start)
 
@@ -275,7 +313,12 @@ def get_chair_features(filename,segment_size):
         next_day = None
         times_seconds = get_seconds(times,next_day)
 
-    start = times_seconds[0]
+    #start = times_seconds[0]
+    tmp = ut.splitall(filename)
+    tmp = tmp[-1]
+    start = tmp[11:-4].split('_')
+    start = float(start[0]) * 3600 + float(start[1]) * 60 + float(start[2])
+
     features = []
     segment_centers = []
 
@@ -286,6 +329,7 @@ def get_chair_features(filename,segment_size):
         cur_A2 = [A2 for i, A2 in enumerate(a2) if times_seconds[i] >=start and times_seconds[i] <= end]
         cur_A3 = [A3 for i, A3 in enumerate(a3) if times_seconds[i] >=start and times_seconds[i] <= end]
         cur_A4 = [A4 for i, A4 in enumerate(a4) if times_seconds[i] >=start and times_seconds[i] <= end]
+
         features.append([mean(cur_A0), stdev(cur_A0), mean(cur_A1), stdev(cur_A1),mean(cur_A2),
                         stdev(cur_A2), mean(cur_A3), stdev(cur_A3), mean(cur_A4), stdev(cur_A4)])
         segment_centers.append(start + segment_size / 2)
@@ -296,48 +340,7 @@ def get_chair_features(filename,segment_size):
     return features, segment_centers, day_start
 
 
-def concat_and_save_features(f1,s1,f2,s2,f3,s3,chair):
-    """Concatenate the given features on timestampt
-
-    Args:
-        f1 (np.array): Mouse Features
-        s1 (np.array): Mouse Seconds
-        f2 (np.array): Keyboard Features
-        s2 (np.array): Keyboard Seconds
-        f3 (np.array): Chair Features
-        s3 (np.array): Chair Seconds
-        chair: Chair Raw .csv file
-    """
-    chair_df = pd.read_csv(chair)
-    chair_df['Time'] = chair_df['Time'].str.split(',').str[0]
-
-    df1 = pd.DataFrame(f1, columns = ['Velocity_X','Velocity_Y','Clicks','R_Clicks','L_Clicks','M_Clicks'])
-    df1['Time'] = convert_seconds(s1)
-
-    df2 = pd.DataFrame(f2, columns = ['All_keys_N','Arrow_keys_N','Spaces_N','Shft_Ctrl_Alt_N'])
-    df2['Time'] = convert_seconds(s2)
-
-    df3 = pd.DataFrame(f3, columns = ['M_A0','STD_A0','M_A1','STD_A1','M_A2','STD_A2','M_A3','STD_A3','M_A4','STD_A4'])
-    df3['Time'] = convert_seconds(s3)
-
-    data_frames = [df1, df2, df3]
-    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Time'],how='outer'), data_frames).fillna('0.0')
-    df_merged.sort_values(by='Time', inplace=True, ascending=True)
-    col = df_merged.pop('Time')
-    df_merged.insert(0, col.name, col)
-    del df1, df2, df3
-
-    df = pd.merge( chair_df, df_merged, on=['Time'])
-    df.drop(['Date','A0','A1','A2','A3','A4'], axis = 1, inplace = True)
-    col = df.pop('Label')
-    df.insert(len(df.columns), col.name, col)
-
-    features_file = ''.join((ut.get_date(),'_',ut.get_time(),'.csv'))
-    features_dir = get_features()
-    features_file = os.path.join(features_dir,features_file)
-    df.to_csv(features_file, sep='\t', encoding='utf-8', index=False)
-
-
+#Todo: Review get_features()
 def get_features() -> str:
     """Get the features path
 
@@ -345,20 +348,164 @@ def get_features() -> str:
         str: The path to save the generated features
     """
     current_path = os.path.abspath(os.getcwd())
-    current_path = os.path.join(current_path,'Data_Samples','Test_Action_1')
+    current_path = os.path.join(current_path,'Debug')
     features_path = os.path.join(current_path,'Features')
     return features_path
 
 
 def extract_features(segment_size):
-    """Extract and save features based on session recordings
+    """Extract
 
     Args:
-        segment_size (int): Segment size(Chunk size) in seconds
+        segment_size (int): Create segments of segmet_size seconds
     """
     mouse,keyboard,chair = get_recordings()
-    for m, k, c in zip(mouse, keyboard, chair):
-        mf,mt,_ = get_mouse_features(m,float(segment_size))
-        kf,kt,_ = get_key_features(k,float(segment_size))
-        cf,ct,_ = get_chair_features(c,float(segment_size))
-        concat_and_save_features(mf,mt,kf,kt,cf,ct,c)
+    pairs = [(i, j, k) for i in mouse for j in keyboard for k in chair if i.split('/')[-1].split('.')[0] == j.split('/')[-1].split('.')[0] == k.split('/')[-1].split('.')[0]]
+
+    for m,k,c in pairs:
+        '''
+        print(m)
+        print(k)
+        print(c)
+        '''
+
+        kf, kt, _ = get_key_features(k,float(segment_size))
+        k_segment_centers = convert_seconds(kt)
+
+        cf, ct, _ = get_chair_features(c,float(segment_size))
+        c_segment_centers = convert_seconds(ct)
+
+        mf, mt, _ = get_mouse_features(m,float(segment_size))
+        m_segment_centers = convert_seconds(mt)
+
+        '''
+        print(mf)
+        print(m_segment_centers)
+
+        print(kf)
+        print(k_segment_centers)
+
+        print(cf)
+        print(c_segment_centers)
+        '''
+
+        compare = lambda x, y, z: collections.Counter(x) == collections.Counter(y) == collections.Counter(z) #compare -> bool
+
+        if compare(k_segment_centers,c_segment_centers,m_segment_centers):
+            segment_centers = c_segment_centers
+
+            #Get the annotated labels from chair recording
+            chair = pd.read_csv(c)
+            chair = chair.drop(['A0','A1','A2','A3','A4'], axis = 1)
+            labels = list(chair['Label'])
+            dates = list(chair['Date'])
+
+            #Drop timestamps miliseconds
+            timestamps = get_seconds(list(chair['Time']),None)
+            timestamps = sorted(timestamps)
+            timestamps = list(convert_seconds(timestamps))
+
+            tmp = []
+            label_idxs = []
+            date_idxs = []
+
+            '''
+            get the common timestamps() and their indexes
+            if a timestamp does not exist in the segments list
+            fill tuple with space
+            '''
+            [tmp.append((sec,timestamps.index(sec))) if sec in timestamps else tmp.append((sec,'')) for i,sec in enumerate(segment_centers)]
+
+            [label_idxs.append(labels[pair[1]]) if pair[1] != '' else label_idxs.append(0) for index,pair in enumerate(tmp)] #Map the labels
+            [date_idxs.append(dates[pair[1]]) if pair[1] != '' else date_idxs.append('NaN') for index,pair in enumerate(tmp)] #Map the Dates
+            del tmp
+
+            #Convert np arrays to dataframes
+            df1 = pd.DataFrame(mf, columns = ['Velocity_X','Velocity_Y','Clicks','R_Clicks','L_Clicks','M_Clicks'])
+            df2 = pd.DataFrame(kf, columns = ['All_keys_N','Arrow_keys_N','Spaces_N','Shft_Ctrl_Alt_N'])
+            df3 = pd.DataFrame(cf, columns = ['M_A0','STD_A0','M_A1','STD_A1','M_A2','STD_A2','M_A3','STD_A3','M_A4','STD_A4'])
+
+            df1['Time'] = m_segment_centers
+            df2['Time'] = k_segment_centers
+            df3['Time'] = c_segment_centers
+
+            '''
+            print(len(df1))
+            print(len(df2))
+            print(len(df3))
+
+            print(df1,'\n')
+            print(df2,'\n')
+            print(df3,'\n')
+            '''
+
+            merge = df1.set_index('Time').join(df2.set_index('Time')).join(df3.set_index('Time'))
+            merge.insert(loc=0, column='Date', value=date_idxs)
+            merge.insert(loc=len(merge.columns), column='Label', value=label_idxs)
+
+            print(merge)
+            print()
+            print(len(merge))
+            print(len(date_idxs))
+            print(len(label_idxs))
+
+            #Save to .csv file
+            #features_file = ''.join((c.split('/')[-1].split('.')[0],'.csv'))
+            #features_dir = get_features()
+            #features_file = os.path.join(features_dir,features_file)
+            #merge.to_csv(features_file, encoding='utf-8', index=False)
+
+            del df1,df2,df3,merge
+
+
+
+def cmp_segments(filename, segment_size):
+    """Check whether the seconds segment size is greater
+       than the recorded second number
+
+    Args:
+        filename (str): The recorded .csv file
+        segment_size (int): A number describing how many second
+                            chunks to examine
+    Returns:
+        bool: True or False
+    """
+    df = pd.read_csv(filename)
+    times = list(df['Time'])
+    day_start = df['Date'][0]
+    next_dates = (df['Date'] > day_start)
+
+    if next_dates.any():
+        next_dates = df.loc[next_dates]
+        next_day = int(next_dates.index[0])
+        times_in_sec = get_seconds(times, next_day)
+    else:
+        next_day = None
+        times_in_sec = get_seconds(times, next_day)
+
+    start_time = times_in_sec[0]
+    end_time = times_in_sec[-1]
+
+    if start_time + float(segment_size) >= end_time:
+        return True
+    return False
+
+
+#TO DO SET DIRECTORY!
+def get_all_features():
+    """Concatenate every .csv file in a directory into a bigger dataframe
+    """
+    current_path = os.path.abspath(os.getcwd())
+    os.chdir('..')
+    current_path = (os.path.abspath(os.curdir))
+    f_path = os.path.join(current_path,'Data_1','Features')
+    abs_path = absoluteFilePaths(f_path)
+
+    list_of_dataframes = []
+    for filename in abs_path:
+        list_of_dataframes.append(pd.read_csv(filename))
+
+    merged_df = pd.concat(list_of_dataframes)
+    merged_df.sort_values(by='Time', inplace=True, ascending=True)
+    newDf = merged_df.dropna(how='any', subset=['Label'])
+    newDf.to_csv('actions.csv',encoding='utf-8', index=False)
