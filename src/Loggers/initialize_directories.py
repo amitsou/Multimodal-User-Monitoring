97cd561976c83028695ab891eb8f85b9539eefbe
amitsou <amitsou95@gmail.com>
2021-05-31 14:59:37 +0300

    Removed Loggers folder

--- src/Loggers/initialize_directories.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "../"))
-from Functions import utils as ut
-
-if __name__=='__main__':
-    ut.initialize_dirs()
--- src/Loggers/scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/home/alex/exit/bin/python3  chairlogger.py &
-/home/alex/exit/bin/python3  keylogger.py &
-/home/alex/exit/bin/python3  mouselogger.py &
-bash webcamlogger.sh
